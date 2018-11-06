c023c833f7ed604c74fb95f18bdc9fc046caaa5d
temskiy <temskiy@gmail.com>
2018-11-06 14:52:29 +0700

    Changed keyfile generation script

--- keys/make_my_keys_dic.sh
+++ keys/make_my_keys_dic.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 cat ../client/default_keys.dic ./*.keys |sed -r '/^#/d'|sed '/^$/d'|awk -F ",|#" '{print $1}'|tr '[:lower:]' '[:upper:]'|sort|uniq > my_keys.dic
+cat my_keys.dic > ../client/default_keys.dic
 
