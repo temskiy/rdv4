4f9288cb064f215e60884d7d0de2b4bc7a49d8f6
temskiy <temskiy@gmail.com>
2020-02-09 20:12:35 +0700

    First working version

--- armsrc/Standalone/lf_em4100emul.c
+++ armsrc/Standalone/lf_em4100emul.c
@@ -21,13 +21,22 @@
 #define MAX_IND 16
 #define CLOCK 64
 
-uint64_t low[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+uint64_t low[] = {0x565AF781C7,0x540053E4E2,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 uint32_t high[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 uint8_t *bba,slots_count;
 int buflen;
+uint64_t buf;
 
 void ModInfo(void) {
     DbpString("  LF EM4100 emulation standalone V1");
+}
+
+uint64_t ReversQuads(uint64_t bits){
+  uint64_t result = 0;
+  for (int i = 0; i < 16; i++){
+    result += ((bits >> (60 - 4 *i)) & 0xf) << (4 * i);
+      }
+  return result >> 24;
 }
 
 void FillBuff(uint8_t bit) {
@@ -37,7 +46,7 @@ void FillBuff(uint8_t bit) {
 	buflen += (CLOCK / 2);
 }
 void ConstructEM410xEmulBuf(uint64_t id) {
-	Dbprintf("buf: %i", id);
+	
     int i, j, binary[4], parity[4];
  	buflen = 0;
     for (i = 0; i < 9; i++)
@@ -46,31 +55,15 @@ void ConstructEM410xEmulBuf(uint64_t id) {
     for (i = 0; i < 10; i++) {
         for (j = 3; j >= 0; j--, id /= 2)
             binary[j] = id % 2;
-        FillBuff(binary[0]);
-        FillBuff(binary[1]);
-        FillBuff(binary[2]);
-        FillBuff(binary[3]);
+        for (j = 0; j < 4; j++)
+			FillBuff(binary[j]);
         FillBuff(binary[0] ^ binary[1] ^ binary[2] ^ binary[3]);
-        parity[0] ^= binary[0];
-        parity[1] ^= binary[1];
-        parity[2] ^= binary[2];
-        parity[3] ^= binary[3];
+        for (j = 0; j < 4; j++)
+			parity[j] ^= binary[j];
     }
-    FillBuff(parity[0]);
-    FillBuff(parity[1]);
-    FillBuff(parity[2]);
-    FillBuff(parity[3]);
+	for (j = 0; j < 4; j++)
+    	FillBuff(parity[j]);
     FillBuff(0);
-}
-
-uint64_t ReversBits(uint64_t bits){
-	uint64_t result = 0;
-	for (int i = 0; i < 64; i++){
-		result *=2;
-		result += bits % 2;
-		bits /= 2;
-	}
-	return result;
 }
 
 void RunMod() {
@@ -80,7 +73,9 @@ void RunMod() {
 	uint8_t state = 0; //0 - idle, 1 - read, 2 - sim
 	slots_count = sizeof(low)/sizeof(low[0]);
 	bba = BigBuf_get_addr();
-	Dbprintf("BigBuf addr: %i", bba );
+	// Dbprintf("BigBuf addr: %x", bba );
+	// Dbprintf("low addr: %x", &low );
+	// Dbprintf("buf addr: %x", &buf );
 	LED(selected, 0);
 	DbpString("[+] now in select mode");
 	for (;;) {		
@@ -99,13 +94,12 @@ void RunMod() {
 					selected = (selected + 1) % slots_count;
 					LEDsoff();
 					LED(selected % MAX_IND, 0);
-					Dbprintf("[=] selected %x slot", selected);
+					Dbprintf("[=] selected slot %x", selected);
 				} 
 			break;
 			case 1:
 				CmdEM410xdemod(1, &high[selected], &low[selected], 0);
-				// sprintf(str, "%02lx%08lx", (uint32_t) (low[selected] >> 32), (uint32_t) low[selected]);
-                Dbprintf("[+] recorded to %i slot", selected);
+                Dbprintf("[+] recorded to slot %x", selected);
 				FlashLEDs(100,5);
 				DbpString("[+] select mode");
 				BigBuf_Clear();
@@ -116,15 +110,15 @@ void RunMod() {
 					SpinDown(100);
 					SpinOff(100);
 					LED(selected % MAX_IND, 0);
-					Dbprintf("[<] read to %x slot", selected);
+					Dbprintf("[<] read to slot %x", selected);
 					state = 1;
 				} else if (button_pressed < 0) {
 					LED(selected % MAX_IND, 0);
-					Dbprintf("[>] prepare for sim from %x slot", selected);
-					ConstructEM410xEmulBuf(ReversBits(low[selected]));
-					Dbprintf("[>] buffer generated from %x slot", selected);
+					Dbprintf("[>] prepare for sim from slot %x", selected);
+					ConstructEM410xEmulBuf(ReversQuads(low[selected]));
+					Dbprintf("[>] buffer generated from slot %x", selected);
 					FlashLEDs(100,5);
-					Dbprintf("[>] simulate from %x slot", selected);
+					Dbprintf("[>] simulate from slot %x", selected);
 					SimulateTagLowFrequency(buflen, 0, 1);
 				}
 			break;
